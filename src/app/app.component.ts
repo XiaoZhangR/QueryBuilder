b786835f6ccb23df7a5bff44da72ee829d3c818a
Xiao Zhang <xz9rc@umsystem.edu>
2022-01-11 16:50:45 -0600

    query-builder init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { FormBuilder, FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
+import { QueryBuilderConfig } from 'angular2-query-builder';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,76 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Query-Builder';
+  // public queryCtrl: FormControl;
+  
+  // public query = {
+  //   condition: 'and',
+  //   rules: [
+  //     {field: 'age', operator: '<=', entity: 'physical'},
+  //     {field: 'birthday', operator: '=', value: new Date(), entity: 'nonphysical'},
+  //     {
+  //       condition: 'or',
+  //       rules: [
+  //         {field: 'gender', operator: '=', entity: 'physical'},
+  //         {field: 'occupation', operator: 'in', entity: 'nonphysical'},
+  //         {field: 'school', operator: 'is null', entity: 'nonphysical'},
+  //         {field: 'notes', operator: '=', entity: 'nonphysical'}
+  //       ]
+  //     }
+  //   ]
+  // };
+
+  public entityConfig: QueryBuilderConfig = {
+    entities: {
+      physical: {name: 'Physical Attributes'},
+      nonphysical: {name: 'Nonphysical Attributes'},
+      test: {name: 'testing'}
+    },
+    fields: {
+      age: {name: 'Age', type: 'number', entity: 'physical'},
+      gender: {
+        name: 'Gender',
+        entity: 'physical',
+        type: 'category',
+        options: [
+          {name: 'Male', value: 'm'},
+          {name: 'Female', value: 'f'}
+        ]
+      },
+      name: {name: 'Name', type: 'string', entity: 'nonphysical'},
+      notes: {name: 'Notes', type: 'textarea', operators: ['=', '!='], entity: 'nonphysical'},
+      educated: {name: 'College Degree?', type: 'boolean', entity: 'nonphysical'},
+      birthday: {name: 'Birthday', type: 'date', operators: ['=', '<=', '>'],
+        defaultValue: (() => new Date()), entity: 'nonphysical'
+      },
+      school: {name: 'School', type: 'string', nullable: true, entity: 'nonphysical'},
+      occupation: {
+        name: 'Occupation',
+        entity: 'nonphysical',
+        type: 'category',
+        options: [
+          {name: 'Student', value: 'student'},
+          {name: 'Teacher', value: 'teacher'},
+          {name: 'Unemployed', value: 'unemployed'},
+          {name: 'Scientist', value: 'scientist'}
+        ]
+      }
+    }
+  };
+
+  // public currentConfig: QueryBuilderConfig;
+  public allowRuleset: boolean = true;
+  // public allowCollapse: boolean;
+  // public persistValueOnFieldChange: boolean = false;
+
+  constructor(
+    // private formBuilder: FormBuilder
+  ) {
+    // this.queryCtrl = this.formBuilder.control(this.query);
+    // this.currentConfig = this.entityConfig;
+  }
+
+  // changeDisabled(event: Event) {
+  //   (<HTMLInputElement>event.target).checked ? this.queryCtrl.disable() : this.queryCtrl.enable();
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,22 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+
+import { QueryBuilderModule } from "angular2-query-builder";
+
 
 @NgModule({
   declarations: [
@@ -10,7 +24,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    QueryBuilderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
